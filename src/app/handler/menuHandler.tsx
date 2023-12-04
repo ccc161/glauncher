bf8131225d6f8e3471bebb1def368b788b069d3a
cc <ccc60138@gmail.com>
2023-12-05 04:25:45 +0800

    suppress error in body style

--- src/app/handler/menuHandler.tsx
+++ src/app/handler/menuHandler.tsx
@@ -1,10 +1,10 @@
 import { Menu } from 'antd';
 import type { MenuItemProps, MenuProps } from 'antd'
 import type {
-    SelectEventHandler,
+    MenuClickEventHandler,
 } from 'rc-menu/lib/interface';
 
-export const clickMainMenuButtonHandler: SelectEventHandler = ({ key, keyPath, selectedKeys, domEvent }) => {
+export const clickMainMenuButtonHandler: MenuClickEventHandler = ({ key, keyPath, domEvent }) => {
     console.log(`Selected key: ${key}`);
     
     // You can perform additional actions based on the selected key
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-array-index-key */
 'use client'
 import React, { useState } from 'react';
 import { Flex, Layout, theme, Divider } from 'antd';
@@ -8,28 +7,32 @@ const { Header, Content, Footer } = Layout;
 import MainMenu from "./menu/mainSidebar"
 import DefaultToolPage from './tool-page/default/page';
 
-const toolContentStyle: React.CSSProperties = {
-  width: "100%",
-  height: "100%",
-};
-
-const WindowStyle: React.CSSProperties = {
-  boxSizing: "border-box",
-  margin: 0,
-}
-
 const App: React.FC = () => {
   const [selectedToolPage, setSelectedToolPage] = useState(DefaultToolPage);
 
   return (
     <>
-      <body style={WindowStyle}>
-        <Layout>
-          <MainMenu />
-          <Content style={toolContentStyle}>
-            {selectedToolPage}
-          </Content>
-        </Layout>
+      <body suppressHydrationWarning>
+        <div style={{
+          width: "100%",
+          maxHeight: "100%",
+          margin: 0,
+          borderLeft: 0,
+          borderRight: 0,
+          borderTop: 0,
+          borderBottom: 0
+        }}>
+          <Layout>
+            <Flex >
+              <MainMenu />
+              <Layout>
+                <Content>
+                  {selectedToolPage}
+                </Content>
+              </Layout>
+            </Flex>
+          </Layout>
+        </div>
       </body>
     </>
   );
